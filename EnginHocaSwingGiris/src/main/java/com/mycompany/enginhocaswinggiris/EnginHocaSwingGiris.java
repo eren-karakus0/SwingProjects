02c0c54f70406203bfa4a86c86848f3c5781b182
Muhammet Eren Karakuş <erenkar950@gmail.com>
2024-06-22 00:41:51 +0300

    SwingJavaProjects

---
+++ EnginHocaSwingGiris/src/main/java/com/mycompany/enginhocaswinggiris/EnginHocaSwingGiris.java
@@ -0,0 +1,8 @@
+package com.mycompany.enginhocaswinggiris;
+
+public class EnginHocaSwingGiris {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ WebSayfasi/src/main/java/com/mycompany/websayfasi/WebSayfasi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.websayfasi;
+
+/**
+ *
+ * @author EREN
+ */
+public class WebSayfasi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
